e6e36f2eef9ad9bebe286c65384f5620e49a67cf
SugarMGP <2350745751@qq.com>
2025-04-06 23:05:50 +0800

    refactor: 重构JWT相关代码

--- internal/pkg/utils/jwt.go
+++ internal/pkg/utils/jwt.go
@@ -9,24 +9,34 @@ import (
 	"github.com/zjutjh/WeJH-SDK/oauth"
 )
 
-var (
-	key string
-	t   *jwt.Token
-)
+var key string
+
+// UserClaims 用户信息
+type UserClaims struct {
+	Name         string `json:"name"`
+	College      string `json:"college"`
+	StudentID    string `json:"stuId"`
+	UserType     string `json:"userType"`
+	UserTypeDesc string `json:"userTypeDesc"`
+	Gender       string `json:"gender"`
+	jwt.RegisteredClaims
+}
 
 // NewJWT 生成 JWT
 func NewJWT(name, college, stuId, userType, userTypeDesc, gender string) string {
 	key = global.Config.GetString("jwt.key")
-	duration := time.Hour * 24 * 7
-	expirationTime := time.Now().Add(duration).Unix() // 设置过期时间
-	t = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"name":         name,
-		"college":      college,
-		"stuId":        stuId,
-		"userType":     userType,
-		"userTypeDesc": userTypeDesc,
-		"gender":       gender,
-		"exp":          expirationTime,
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaims{
+		Name:         name,
+		College:      college,
+		StudentID:    stuId,
+		UserType:     userType,
+		UserTypeDesc: userTypeDesc,
+		Gender:       gender,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(24*7) * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
 	})
 	s, err := t.SignedString([]byte(key))
 	if err != nil {
@@ -38,40 +48,25 @@ func NewJWT(name, college, stuId, userType, userTypeDesc, gender string) string 
 // ParseJWT 解析 JWT
 func ParseJWT(token string) (oauth.UserInfo, error) {
 	key = global.Config.GetString("jwt.key")
-	t, err := jwt.Parse(token, func(_ *jwt.Token) (any, error) {
+	t, err := jwt.ParseWithClaims(token, &UserClaims{}, func(_ *jwt.Token) (any, error) {
 		return []byte(key), nil
 	})
 	if err != nil {
 		return oauth.UserInfo{}, err
 	}
-	claims, ok := t.Claims.(jwt.MapClaims)
-	if !ok || !t.Valid { // 检查令牌是否有效
+
+	userClaims, ok := t.Claims.(*UserClaims)
+	if !ok || !t.Valid {
 		return oauth.UserInfo{}, errors.New("invalid token")
 	}
 
-	// 验证 exp 是否有效
-	if exp, ok := claims["exp"].(float64); ok {
-		if time.Unix(int64(exp), 0).Before(time.Now()) {
-			return oauth.UserInfo{}, errors.New("token expired")
-		}
-	}
-
-	keys := []string{"name", "college", "stuId", "userType", "userTypeDesc", "gender"}
-	values := make(map[string]string)
-	for _, k := range keys {
-		v, ok := claims[k].(string)
-		if !ok {
-			return oauth.UserInfo{}, errors.New("invalid token")
-		}
-		values[k] = v
-	}
 	userInfo := oauth.UserInfo{
-		Name:         values["name"],
-		College:      values["college"],
-		UserType:     values["userType"],
-		UserTypeDesc: values["userTypeDesc"],
-		Gender:       values["gender"],
-		StudentID:    values["stuId"],
+		Name:         userClaims.Name,
+		College:      userClaims.College,
+		UserType:     userClaims.UserType,
+		UserTypeDesc: userClaims.UserTypeDesc,
+		Gender:       userClaims.Gender,
+		StudentID:    userClaims.StudentID,
 	}
 	return userInfo, nil
 }
